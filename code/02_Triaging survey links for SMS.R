bcfcb23da84ceb9cce02c37b69582105011db95c
Merryn Roe <merryn.s.roe@gmail.com>
2026-03-25 15:22:03 +1100

    Minor updates

--- code/02_Triaging survey links for SMS.R
+++ code/02_Triaging survey links for SMS.R
@@ -42,7 +42,7 @@ contact_dat <- redcap %>%
   select(
     record_id,
     sms_consent,
-    phone,
+    contact_number,
     first_name, 
     exposure_ip1_yn, 
     exposure_ip2_yn
@@ -53,7 +53,7 @@ contact_dat <- redcap %>%
 
 # REDCap unique survey link
 # Download from survey distribution tools, ensure it is the correct survey and you add '_triage_link' to doc name to avoid confusion
-link_dat <- read.csv(here::here("raw_data", "HPAISurvey_Participants_2025-11-26_1055_triage_link.csv")) # Update with most recent version
+link_dat <- read.csv(here::here("raw_data", "HPAISurvey_Participants_2026-03-25_1418_triage_link.csv")) # Update with most recent version
 
 link_dat <- link_dat %>%
   clean_names() %>%
@@ -67,11 +67,11 @@ merged_data <- left_join(contact_dat, link_dat, by = "record_id")
 # Filter to send triage out if any triage exposure Qs are missing, this should capture when we create new site/exposures
 # Final essendex list
 
-essendex <- merged_data %>%
+genesis <- merged_data %>%
   #filter(is.na(exposure_ip1_yn) | is.na(exposure_ip2_yn)) %>% # Use this when second IP included
   filter(is.na(exposure_ip1_yn)) %>%
-  select(first_name, phone, survey_link) %>%
-  mutate(phone = paste0("+", phone)) # Format phone number for essendex
+  select(first_name, contact_number, survey_link) %>%
+  mutate(contact_number = paste0("+", contact_number)) # Format phone number for essendex
 
-write.csv(essendex, file = here::here("outputs", paste0("sms_list_triage_", format(Sys.time(), "%Y%m%d"), ".csv")), row.names = FALSE)
+write.csv(genesis, file = here::here("outputs", paste0("sms_list_triage_", format(Sys.time(), "%Y%m%d"), ".csv")), row.names = FALSE)
 
