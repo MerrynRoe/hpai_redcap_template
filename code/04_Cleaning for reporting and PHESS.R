9c4f97d967da4efeadfdef079db1b8ccc4ebb4a7
Merryn Roe <merryn.s.roe@gmail.com>
2026-03-26 08:00:17 +1100

    IP2 risk assessment code

--- code/04_Cleaning for reporting and PHESS.R
+++ code/04_Cleaning for reporting and PHESS.R
@@ -168,6 +168,126 @@ dat_clean <- redcap %>%
     ~ "Negligible Risk",
     
     TRUE ~ NA_character_
+  ),
+  # IP 2
+  exposure_risk_calculated_ip2 = case_when(
+    # ----------------------
+    # 🔴 HIGH RISK
+    # ----------------------
+    high_risk_ppe_ip2 %in% c("2", "3") |
+      high_risk_ppe_breach_ip2 %in% c("1", "3") |
+      high_risk_ppe_removal_ip2 %in% c("2", "3") |
+      
+      contact_animals_ppe_ip2 %in% c("2", "3") |
+      contact_animals_ppe_breach_ip2 %in% c("1", "3") |
+      contact_animals_ppe_removal_ip2 %in% c("2", "3") |
+      
+      contact_objects_ppe_ip2 %in% c("2", "3") |
+      contact_objects_ppe_breach_ip2 %in% c("1", "3") |
+      contact_objects_ppe_removal_ip2 %in% c("2", "3") |
+      
+      contact_other_ppe_ip2 %in% c("2", "3") |
+      contact_other_ppe_breach_ip2 %in% c("1", "3") |
+      contact_other_ppe_removal_ip2 %in% c("2", "3")
+    ~ "High Risk",
+    
+    
+    # ----------------------
+    # 🟠 LOW RISK (protected direct/high-risk contact)
+    # ----------------------
+    (
+      high_risk_activities_ip2 %in% c("1", "3") &
+        (high_risk_ppe_ip2 == "1" |
+           high_risk_ppe_breach_ip2 == "2" |
+           high_risk_ppe_removal_ip2 == "1")
+    ) |
+      
+      (
+        contact_animals_ip2 %in% c("1", "3") &
+          (contact_animals_ppe_ip2 == "1" |
+             contact_animals_ppe_breach_ip2 == "2" |
+             contact_animals_ppe_removal_ip2 == "1")
+      ) |
+      
+      (
+        contact_objects_ip2 %in% c("1", "3") &
+          (contact_objects_ppe_ip2 == "1" |
+             contact_objects_ppe_breach_ip2 == "2" |
+             contact_objects_ppe_removal_ip2 == "1")
+      ) |
+      
+      (
+        contact_other_ip2 %in% c("1", "3") &
+          (contact_other_ppe_ip2 == "1" |
+             contact_other_ppe_breach_ip2 == "2" |
+             contact_other_ppe_removal_ip2 == "1")
+      )
+    ~ "Low Risk",
+    
+    
+    # ----------------------
+    # 🟡 LOW RISK (vicinity exposure)
+    # ----------------------
+    vicinity_animals_ppe_ip2 %in% c("2", "3") |
+      vicinity_animals_ppe_breach_ip2 %in% c("1", "3") |
+      vicinity_animals_ppe_removal_ip2 %in% c("2", "3") |
+      
+      vicinity_objects_ppe_ip2 %in% c("2", "3") |
+      vicinity_objects_ppe_breach_ip2 %in% c("1", "3") |
+      vicinity_objects_ppe_removal_ip2 %in% c("2", "3") |
+      
+      vicinity_other_ppe_ip2 %in% c("2", "3") |
+      vicinity_other_ppe_breach_ip2 %in% c("1", "3") |
+      vicinity_other_ppe_removal_ip2 %in% c("2", "3") |
+      
+      vicinity_exposure_time_ip2 == "1"
+    ~ "Low Risk",
+    
+    
+    # ----------------------
+    # 🟢 NEGLIGIBLE RISK
+    # ----------------------
+    (
+      high_risk_activities_ip2 == "2" &
+        contact_animals_ip2 == "2" &
+        contact_objects_ip2 == "2" &
+        contact_other_ip2 == "2"
+    ) &
+      
+      (
+        (
+          vicinity_animals_ip2 == "2" &
+            vicinity_objects_ip2 == "2" &
+            vicinity_other_ip2 == "2"
+        ) |
+          
+          (
+            (
+              vicinity_animals_ip2 %in% c("1", "3") &
+                vicinity_animals_ppe_ip2 == "1" &
+                vicinity_animals_ppe_breach_ip2 == "2" &
+                vicinity_animals_ppe_removal_ip2 == "1"
+            ) |
+              
+              (
+                vicinity_objects_ip2 %in% c("1", "3") &
+                  vicinity_objects_ppe_ip2 == "1" &
+                  vicinity_objects_ppe_breach_ip2 == "2" &
+                  vicinity_objects_ppe_removal_ip2 == "1"
+              ) |
+              
+              (
+                vicinity_other_ip2 %in% c("1", "3") &
+                  vicinity_other_ppe_ip2 == "1" &
+                  vicinity_other_ppe_breach_ip2 == "2" &
+                  vicinity_other_ppe_removal_ip2 == "1"
+              )
+          ) &
+          vicinity_exposure_time_ip2 == "0"
+      )
+    ~ "Negligible Risk",
+    
+    TRUE ~ NA_character_
   )
   )
 
@@ -176,8 +296,28 @@ dat_clean %>%
   select(record_id,  exposure_risk_assessment_ip1, exposure_risk_calculated_ip1, exposure_risk_assessment_ip2) %>%
   view()
 
-## TODO:
-# Create a exposure_risk_calculated_all variable to cover both ips
-
 # Export QA list of REDCap ID where exposure_risk_assessment_ip1 =! exposure_risk_calculated_ip1 for each IP to send to ops manager
+dat_clean <- dat_clean %>%
+  mutate(
+    exposure_risk_qa_ip1 = case_when(
+      exposure_risk_assessment_ip1 == 0 & exposure_risk_calculated_ip1 != "High Risk" ~ "Risk assessment mismatch",
+      exposure_risk_assessment_ip1 == 1 & exposure_risk_calculated_ip1 != "Low Risk" ~ "Risk assessment mismatch",
+      exposure_risk_assessment_ip1 == 2 & exposure_risk_calculated_ip1 != "Negligible Risk" ~ "Risk assessment mismatch",
+      TRUE ~ NA_character_
+  ),
+    exposure_risk_qa_ip2 = case_when(
+      exposure_risk_assessment_ip2 == 0 & exposure_risk_calculated_ip2 != "High Risk" ~ "Risk assessment mismatch",
+      exposure_risk_assessment_ip2 == 1 & exposure_risk_calculated_ip2 != "Low Risk" ~ "Risk assessment mismatch",
+      exposure_risk_assessment_ip2 == 2 & exposure_risk_calculated_ip2 != "Negligible Risk" ~ "Risk assessment mismatch",
+      TRUE ~ NA_character_
+  ))
 
+# Check
+dat_clean %>%
+  select(record_id,  exposure_risk_assessment_ip1, exposure_risk_calculated_ip1, exposure_risk_qa_ip1, exposure_risk_assessment_ip2, exposure_risk_calculated_ip2, exposure_risk_qa_ip2) %>%
+  view()
+
+## To do
+# Confirmed cases
+# exposure_ip1_yn exposure_ip2_yn == 1
+# test_results == 2 or == 3 
