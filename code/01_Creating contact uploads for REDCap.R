0b62167100a831ed661ea356275e4104ead0f8d0
Merryn Roe <merryn.s.roe@gmail.com>
2026-03-25 14:19:20 +1100

    Debugging triage part 1

--- code/01_Creating contact uploads for REDCap.R
+++ code/01_Creating contact uploads for REDCap.R
@@ -11,7 +11,7 @@ pacman::p_load(
 )
 
 # Import caselist - ensure most upto date version
-dat_contacts_facility_ip1 <- read.csv(here::here("raw_data", "20260324_facilty_ip1_contact_list.csv"))
+dat_contacts_facility_ip1 <- read.csv(here::here("raw_data", "test_AI_contact_upload_template_20260325.csv"))
 
 # Import REDCap case list
 source(here::here("code", "api_tokens.R"))
@@ -39,34 +39,42 @@ redcap <- httr::content(response)
 redcap <- redcap %>%
   select(record_id, first_name, phone, exposure_ip1_yn, exposure_ip1_yn)
 
-max_id <- max(redcap$record_id, na.rm = TRUE) # We will need this to add record_id numbers
+# Collect the maximum Record ID to assign new Record IDs for new contacts
+max_id <- if (length(redcap$record_id) == 0 || all(is.na(redcap$record_id))) {
+  0
+} else {
+  max(redcap$record_id, na.rm = TRUE) #if no values (ie first import) = 0
+}
 
 # Match on key fields, ie first name and mobile number
 
 dat_contacts_facility_ip1 <- dat_contacts_facility_ip1 %>%
-  mutate(contact_list_flag_ip1 = 1) %>% ### Create contact list flag, ensure variable is updated per facility ###
+  mutate(case_list_ip1_yn = 1) %>% ### Create contact list flag, ensure variable is updated per facility ###
   mutate(
-    phone_clean = phone %>%
+    contact_number = phone %>%
       str_remove_all("[^0-9]") %>%          # remove spaces, +, brackets, etc.
       str_replace("^0", "") %>%             # drop leading 0 (e.g., 04...)
       str_replace("^61", "61") %>%          # ensure consistent 61 prefix
       { ifelse(str_starts(., "4"), paste0("61", .), .) } # add "61" if missing
   ) %>%
-  mutate(first_name_clean = str_to_lower(first_name))
+  mutate(contact_name = str_to_lower(first_name)) %>%
+  select(contact_name, contact_number, email)
 
 redcap <- redcap %>%
   mutate(
-    phone_clean = phone %>%
+    ## Todo clean entries with spaces ie 0401 088 851
+    contact_number = phone %>%
       str_remove_all("[^0-9]") %>%          # remove spaces, +, brackets, etc.
       str_replace("^0", "") %>%             # drop leading 0 (e.g., 04...)
       str_replace("^61", "61") %>%          # ensure consistent 61 prefix
       { ifelse(str_starts(., "4"), paste0("61", .), .) } # add "61" if missing
   ) %>%
-  mutate(first_name_clean = str_to_lower(first_name)) 
+  mutate(contact_name = str_to_lower(first_name)) %>%
+  select(contact_name, contact_number, record_id, exposure_ip1_yn)
 
 dat_contacts_facility_ip1 <- dat_contacts_facility_ip1 %>%
   left_join(redcap,
-            by = c("first_name_clean", "phone_clean")) 
+            by = c("contact_name", "contact_number")) # Matches by phone and contact number
 
 # Only upload new cases
 dat_contacts_facility_ip1_new <- dat_contacts_facility_ip1 %>%
@@ -74,23 +82,25 @@ dat_contacts_facility_ip1_new <- dat_contacts_facility_ip1 %>%
 
 dat_contacts_facility_ip1_new <- dat_contacts_facility_ip1_new %>%
    mutate(
-    record_id = if_else(
+    record_id = if_else(       # Create new REDCap ID adding to the last value of pulled data
       is.na(record_id),
       max_id + row_number(),   # sequential values
       record_id
     )
   ) %>%
-  select(-phone.x, -first_name.x, -phone.y, -first_name.y) %>%
-  rename(phone = phone_clean,
-         first_name = first_name_clean) %>%
-  mutate(first_name = str_to_title(first_name)) %>%   # Capitalise first letter
-  select(record_id, everything())
+  mutate(contact_name = str_to_title(contact_name)) %>%   # Capitalise first letter
+  mutate(email = case_when(
+    is.na(email) ~ "PHUepi.analytics@gvhealth.org.au", # email can't be missing to get a unique link, using EPI email as default
+    email == "" ~ "PHUepi.analytics@gvhealth.org.au",
+    !is.na(email) ~ email
+  )) %>%
+  select(record_id, everything(), -exposure_ip1_yn)
 
 # Export .csv to import into REDCap
 timestamp <- format(Sys.time(), "%Y-%m-%d_%H-%M-%S")
 
 write.csv(
-  dat_contacts_facility_a_new,
+  dat_contacts_facility_ip1_new,
   here::here("outputs", paste0(timestamp, "_import_to_redcap_for_triage.csv")),
   row.names = FALSE
 )
